d86be7905654e65ef414dd989d7186504d00749e
Ryan Collingham <ryanc118@gmail.com>
2018-06-23 20:07:00 +0100

    Retrieve memes from multiple users (well 2)

--- artsymemesbot.py
+++ artsymemesbot.py
@@ -1,23 +1,32 @@
 #!/usr/bin/env python3
-"""Bot to post the freshest memes from the Louvre."""
+"""Bot backend to find the freshest memes from the Louvre."""
 import tweepy
 import random
 import os
+import sys
 
-CONSUMER_KEY = os.environ['TWTR_CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['TWTR_CONSUMER_SECRET']
-ACCESS_TOKEN = os.environ['TWTR_ACCESS_TOKEN']
-ACCESS_TOKEN_SECRET = os.environ['TWTR_ACCESS_TOKEN_SECRET']
+try:
+    CONSUMER_KEY = os.environ['TWTR_CONSUMER_KEY']
+    CONSUMER_SECRET = os.environ['TWTR_CONSUMER_SECRET']
+    ACCESS_TOKEN = os.environ['TWTR_ACCESS_TOKEN']
+    ACCESS_TOKEN_SECRET = os.environ['TWTR_ACCESS_TOKEN_SECRET']
+except KeyError:
+    print('Error, you must set each of TWTR_CONSUMER_KEY,\n'
+          'TWTR_CONSUMER_SECRET, TWTR_ACCESS_TOKEN and\n'
+          'TWTR_ACCESS_TOKEN_SECRET envronment variables.')
+    sys.exit(1)
+
+USERNAMES = ('ClassicArtMeme', 'classicartmemes')
 
 
-def get_image_urls():
+def get_image_urls(username):
     """Call into the twitter API to retrieve the URLs of some dank memes."""
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 
     api = tweepy.API(auth)
 
-    public_tweets = api.user_timeline("ClassicArtMeme")
+    public_tweets = api.user_timeline(username)
 
     image_urls = []
     for tweet in public_tweets:
@@ -34,10 +43,12 @@ def get_image_urls():
 
 def get_random_image():
     """Retrieve a URL of a single top quality meme."""
-    dank_memes = get_image_urls()
+    meme_source = random.choice(USERNAMES)
+    dank_memes = get_image_urls(meme_source)
     return random.choice(dank_memes)
 
 
 if __name__ == '__main__':
     print(get_random_image())
+    sys.exit(0)
 
